fb92681c0cabd1d505360e3ca60b7d75fbfc5a6d
Sambhu <rajsamb@hotmail.com>
2018-05-19 15:18:53 +0100

    [FEATURE] - Initial npm package

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# unicode-typing
+# Unicode Typing
 Returns the mapped unicode key based.
+
+##Command
+
+"compile": "babel --source-maps -d lib/ src/"
+
+npm run compile: Bable will scan all files in the src directory, transpile your ES6 code to ES5, and store the result in the lib directory
+
+The --source-map flag is added to generte source maps which help you with debugging code
+
+"prepublish": "npm run compile"
+This tells NPM to compile our code before it is published to the repository.
+
+
+prepublish: is a special script that is executed automatically when you publish a package to the NPM repository. In this script we define all the steps that have to be executed before we can publish our package. In our case, this is simple:
+
+
+######Source:
+https://www.wouterbulten.nl/blog/tech/publishing-es6-package-to-npm/
+
+https://www.npmjs.com/package/es2015-packages-best-practices-t
---
+++ src/UnicodeTyping.js
@@ -0,0 +1,157 @@
+class UnicodeTyping {
+
+    constructor(keyEvent, options = {}) {                
+        Object.assign(this, {                    
+            keyMapping: {
+                'K5_65_A': {
+                    n: 'normal',
+                    c: 'ctrl',
+                    s: 'shift',
+                    c_a: 'ctrl_alt',
+                    c_a_s: 'ctrl_alt_shift',
+                }
+            }
+        }, options);
+        
+        this.keyEvent = keyEvent;
+    }
+
+    getUnicodeKey() {
+        // console.log(keyEvent.keyShortName);
+        // console.log(this.keyMapping[keyEvent.keyShortName]);                                
+        
+        let keyObject = this.keyMapping[keyEvent.keyShortName];                
+
+        if (keyObject) {
+            if (this.isCtrlAltShift()) {
+                return keyObject['c_a_s'];
+            }
+
+            if (this.isCtrlAlt()) {                        
+                return keyObject['c_a'];
+            }
+
+            if (this.isShift()) {                        
+                return keyObject['s'];
+            }
+
+            /**
+             * In case, if it is ctrl combo key, for e.g. ctrl + A, ctrl + c, it will
+             * return false which will bypass e.prevent default and hence will use
+             * default action. For e.g. Ctrl + A will select; ctrl + c will copy
+             * and so on
+             */
+            if (this.isSpecialCtrlComboKey()) {
+                return false;
+            }
+
+            /*
+             * In Case of ctrl return normal key.
+             */
+            if (this.isCtrl()) {                        
+                return keyObject['n'];
+            }                    
+
+            if (this.keyEvent.ctrlKey === false && this.keyEvent.shiftKey === false) {
+                return keyObject['n'];
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Checks if it is Special Ctrl Combo Key. For e.g.,
+     * Ctrl + A, Ctrl + S
+     *
+     * @TODO: Check ctrl + uppercase combination
+     */
+    isSpecialCtrlComboKey() {
+        return this.isSelectAll() ||
+                this.isCopy() ||
+                this.isCut() ||
+                this.isPaste() ||
+                this.isFind() ||
+                this.isFindNext() ||
+                this.isRefresh() ||
+                this.isHistory() ||
+                this.isOpen() ||
+                this.isSave() ||
+                this.isPrint() ||
+                this.isNewBrowser() ||
+                this.isBookmark() ||
+                this.isSelectSearchBox();
+    }
+
+    isSelectAll() {
+        return this.keyEvent.ctrlKey === true && this.keyEvent.key.toLowerCase() === 'a';
+    }
+
+    isCopy() {
+        return this.keyEvent.ctrlKey === true && this.keyEvent.key.toLowerCase() === 'c';
+    }
+
+    isCut() {
+        return this.keyEvent.ctrlKey === true && this.keyEvent.key.toLowerCase() === 'x';
+    }
+
+    isPaste() {
+        return this.keyEvent.ctrlKey === true && this.keyEvent.key.toLowerCase() === 'v';
+    }
+
+    isFind() {
+        return this.keyEvent.ctrlKey === true && this.keyEvent.key.toLowerCase() === 'f';
+    }
+
+    isFindNext() {
+        return this.keyEvent.ctrlKey === true && this.keyEvent.key.toLowerCase() === 'g';
+    }
+
+    isRefresh() {
+        return this.keyEvent.ctrlKey === true && this.keyEvent.key.toLowerCase() === 'r';
+    }
+
+    isHistory() {
+        return this.keyEvent.ctrlKey === true && this.keyEvent.key.toLowerCase() === 'h';
+    }
+
+    isOpen() {
+        return this.keyEvent.ctrlKey === true && this.keyEvent.key.toLowerCase() === 'o';
+    }
+
+    isSave() {
+        return this.keyEvent.ctrlKey === true && this.keyEvent.key.toLowerCase() === 's';
+    }
+
+    isPrint() {
+        return this.keyEvent.ctrlKey === true && this.keyEvent.key.toLowerCase() === 'p';
+    }
+
+    isNewBrowser() {
+        return this.keyEvent.ctrlKey === true && this.keyEvent.key.toLowerCase() === 'n';
+    }
+
+    isBookmark() {
+        return this.keyEvent.ctrlKey === true && this.keyEvent.key.toLowerCase() === 'd';
+    }
+
+    isSelectSearchBox() {
+        return this.keyEvent.ctrlKey === true && this.keyEvent.key.toLowerCase() === 'e';
+    }
+
+    isCtrlAltShift() {
+        return this.keyEvent.ctrlKey === true && this.keyEvent.altKey === true && this.keyEvent.shiftKey === true;
+    }
+
+    isCtrlAlt() {
+        return this.keyEvent.ctrlKey === true && this.keyEvent.altKey === true;
+    }
+
+    isShift() {
+        return this.keyEvent.shiftKey === true;
+    }
+
+    isCtrl() {
+        return this.keyEvent.ctrlKey === true;
+    }
+}
